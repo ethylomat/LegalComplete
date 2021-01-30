4939aa9e45eea04818445ef70c87c2ea15e05d90
Lennart S <lennart@c6h12o6.de>
2021-01-30 17:05:15 +0100

    Add support for triggering suggestions

--- src/completion_n_gram.py
+++ src/completion_n_gram.py
@@ -19,8 +19,17 @@ from src.utils.stopwords import stopwords
 
 
 def clean(sentence):
+    """
+    Function to clean punctuation, numbers and symbols from SpaCy sentences. Is applied to Pandas dataframe.
+
+    Args:
+        sentence: input sentence (SpaCy doc)
+    Returns:
+        cleaned: list of words (not SpaCy doc) not containing punctuation, numbers, symbols
+    """
     tokens_to_remove = {"PUNCT", "NUM", "SYM"}
-    return list(filter(lambda tok: tok.pos_ not in tokens_to_remove, sentence))
+    cleaned = list(filter(lambda tok: tok.pos_ not in tokens_to_remove, sentence))
+    return cleaned
 
 
 def clean_ngram(ngram):
@@ -46,6 +55,19 @@ def find_ngram(sentence, reference, n: int, stopwords):
                 return None
 
 
+def find_norm_bigram(sentence, reference):
+    for i, word in enumerate(sentence):
+        if word.ent_type_ == "SECTION_REFERENCE" and word.ent_iob_ == "B":
+            try:
+                sentence_start = sentence[:i]
+                ngram = [word for word in sentence_start]
+                return clean_ngram(ngram)[-1:] + [reference]
+
+            # Reference in the beginning of the line
+            except IndexError:
+                return None
+
+
 class NGramCompletion(Completion):
     """
     Class for n-gram completion of references
@@ -57,6 +79,7 @@ class NGramCompletion(Completion):
     sentence_reference_test = None
     sentence_reference_dev = None
     bigram_counts = None
+    bigram_reference_probs = None
     wordlist = None
 
     def __init__(self):
@@ -85,12 +108,14 @@ class NGramCompletion(Completion):
         self.find_ngrams(self.sentence_reference_test)
 
         self.find_bigrams(self.sentence_reference_train)
-        self.find_bigrams(self.sentence_reference_dev)
+        self.find_bigrams(self.sentence_reference_dev, test=True)
         self.find_bigrams(self.sentence_reference_test, test=True)
 
     @timer
     def find_ngrams(self, df):
         df["cleaned"] = df["sentence"].apply(clean)
+
+        # n-grams to determine norm reference
         df["ngram"] = df.apply(
             lambda row: find_ngram(
                 sentence=row["cleaned"],
@@ -101,17 +126,38 @@ class NGramCompletion(Completion):
             axis=1,
         )
 
+        # n-grams to determine the presence of norm reference
+        df["ngram no sw"] = df.apply(
+            lambda row: find_ngram(
+                sentence=row["cleaned"],
+                reference=row["reference"],
+                n=5,
+                stopwords=[],
+            ),
+            axis=1,
+        )
+
     @timer
     def find_bigrams(self, df, test=False):
+        # TODO: Refactoring (-> DRY, with/without sw)
+
         bigrams = []
+        bigrams_no_sw = []
 
         if not self.wordlist:
             self.wordlist = []
 
-        for ngram in tqdm(df["ngram"], desc="Building bigrams ..."):
+        for ngram in tqdm(df["ngram"], desc="Building bigrams (with stopwords) ..."):
             self.wordlist += ngram
             for i in range(len(ngram) - 1):
                 bigrams.append((ngram[i], ngram[i + 1]))
+
+        for ngram in tqdm(
+            df["ngram no sw"], desc="Building bigrams (without stopwords) ..."
+        ):
+            self.wordlist += ngram
+            for i in range(len(ngram) - 1):
+                bigrams_no_sw.append((ngram[i], ngram[i + 1]))
 
         self.wordlist = list(set(self.wordlist))
 
@@ -131,17 +177,53 @@ class NGramCompletion(Completion):
         if not self.bigram_counts:
             self.bigram_counts = {}
 
+        if not self.bigram_reference_probs:
+            self.bigram_reference_probs = {}
+
         if not test:
             # Initializing dict of bigrams with
             # key: first item of bigrams
             # item: list of secondary items of bigrams
             bigram_dict = {bigram[0]: [] for bigram in bigrams}
-            for bigram in tqdm(bigrams, desc="Counting bigrams ..."):
+            for bigram in tqdm(bigrams, desc="Counting bigrams (with stopwords) ..."):
                 bigram_dict[bigram[0]].append(bigram[1])
+
+            bigrams_no_sw_dict = {bigram[0]: [] for bigram in bigrams_no_sw}
+            for bigram in tqdm(
+                bigrams_no_sw, desc="Counting bigrams (without stopwords) ..."
+            ):
+                bigrams_no_sw_dict[bigram[0]].append(bigram[1])
 
             # Applying Count dictionary (collections.Count()) on secondary items list
             for first, secondaries in bigram_dict.items():
                 self.bigram_counts[first] = Counter(secondaries)
+
+            for first, secondaries in bigrams_no_sw_dict.items():
+                reference_count = len(
+                    [
+                        secondary
+                        for secondary in secondaries
+                        if secondary.startswith("§")
+                    ]
+                )
+                no_reference_count = len(
+                    [
+                        secondary
+                        for secondary in secondaries
+                        if not secondary.startswith("§")
+                    ]
+                )
+
+                try:
+                    prob = reference_count / no_reference_count
+                except ZeroDivisionError:
+                    prob = 0
+
+                self.bigram_reference_probs[first] = {
+                    "ref": reference_count,
+                    "noref": no_reference_count,
+                    "prob": prob,
+                }
 
     def get_bigram_prob(self, bigram):
         try:
@@ -162,42 +244,117 @@ class NGramCompletion(Completion):
         return prob
 
     def get_suggestions(self, ngram):
+        """
+        Method to receive n-gram based norm reference suggestions.
+
+        Args:
+            ngram: n-1 gram input for suggestion
+        Returns:
+            probs: sorted list of top 100 (probability, word) pairs for suggestion. Probability is currently not normed.
+        """
         ngram = [x.lower() for x in ngram if x not in self.stopwords]
         probs = []
         for word in [word for word in self.wordlist if "§" in word]:
             prob = self.get_ngram_prob(ngram + [word])
             if prob != 0 and "§" in word:
                 probs.append((prob, word))
-        return sorted(probs)[::-1][:100]
+        probs = sorted(probs)[::-1][:100]
+        return probs
+
+    def get_trigger_prob(self, ngram):
+        """
+        Method to receive probability to trigger suggestion event.
+
+        Args:
+            ngram: n-1 gram input for suggestion probability
+        Returns:
+            prob: probability of triggering suggestion (probability of section references for bigram)
+        """
+        ngram = [x.lower() for x in ngram]
+
+        if len(ngram) > 0:
+            last_word = ngram[-1]
+        else:
+            return 0
+
+        if last_word.startswith("§"):
+            return 1
+        try:
+            prob = self.bigram_reference_probs[last_word]["prob"]
+        except KeyError:
+            prob = 0
+        return prob
 
     @timer
     def evaluate(self):
+        """
+        Method for evaluation of suggestions. Prints the amounts of correct suggestions based on the test set.
+        Considering the suggestion being suggested at the top 3 suggestions.
+        """
+        TRIGGER_THRESHOLD = 0.5
+
         first = 0
         three = 0
         incorrect = 0
         failed = 0
 
+        triggered_first = 0
+        triggered_three = 0
+        triggered_incorrect = 0
+        triggered_failed = 0
+
+        correct_trigger = 0
+        false_trigger = 0
+        overall_trigger = 0
+
         print("\nEvaluating ...")
-        for test_ngram in tqdm(
-            self.sentence_reference_test["ngram"], desc="Evaluation"
-        ):
-            x = test_ngram[:-1]  # Input (n-1)-gram
-            y = test_ngram[-1:][0]  # Output 1-gram
+        for test_sample in tqdm(self.sentence_reference_test.iloc, desc="Evaluation"):
+            trigger_prob = self.get_trigger_prob(test_sample["ngram no sw"][-2:-1])
+
+            x = test_sample["ngram"][:-1]  # Input (n-1)-gram
+            y = test_sample["ngram"][-1:][0]  # Output 1-gram
 
             # Top 3 suggestions
             suggestions = [suggestion[1] for suggestion in self.get_suggestions(x)[:3]]
             if len(suggestions) > 0:
                 if y == suggestions[0]:
                     first += 1
+                    if trigger_prob >= TRIGGER_THRESHOLD:
+                        triggered_first += 1
                 if y in suggestions:
                     three += 1
+                    if trigger_prob >= TRIGGER_THRESHOLD:
+                        triggered_three += 1
                 else:
                     incorrect += 1
+                    if trigger_prob >= TRIGGER_THRESHOLD:
+                        triggered_incorrect += 1
             else:
                 failed += 1
+                if trigger_prob >= TRIGGER_THRESHOLD:
+                    triggered_failed += 1
 
+            sample = test_sample["ngram no sw"]
+            bigrams = zip(sample[:-1], sample[1:])
+
+            for bigram in bigrams:
+                x = bigram[:1]
+                y = bigram[-1:]
+
+                trigger_prob = self.get_trigger_prob(x)
+                if trigger_prob >= TRIGGER_THRESHOLD:
+                    if y[0].startswith("§"):
+                        correct_trigger += 1
+                    else:
+                        false_trigger += 1
+
+        overall_trigger = correct_trigger + false_trigger
         overall_count = three + incorrect + failed
+        overall_count_triggered = (
+            triggered_three + triggered_incorrect + triggered_failed
+        )
 
+        # Printing the results
         print()
         table = Table(
             title="Evaluation results:",
@@ -217,4 +374,36 @@ class NGramCompletion(Completion):
             "[red]incorrect", f"{incorrect} ({incorrect / overall_count:2.5f})"
         )
         table.add_row("failed", f"{failed} ({failed / overall_count:2.5f})")
+
+        table.add_row("", "")
+        table.add_row(
+            "correct triggered",
+            f"{correct_trigger} ({correct_trigger / overall_trigger:2.5f})",
+        )
+        table.add_row(
+            "false triggered",
+            f"{false_trigger} ({false_trigger / overall_trigger:2.5f})",
+        )
+        table.add_row(
+            "overall triggered",
+            f"{overall_trigger} ({overall_trigger / overall_trigger:2.5f})",
+        )
+
+        table.add_row("", "")
+        table.add_row(
+            "triggered, first",
+            f"{triggered_first} ({triggered_first / overall_count_triggered:2.5f})",
+        )
+        table.add_row(
+            "triggered, three",
+            f"{triggered_three} ({triggered_three / overall_count_triggered:2.5f})",
+        )
+        table.add_row(
+            "triggered, incorrect",
+            f"{triggered_incorrect} ({triggered_incorrect / overall_count_triggered:2.5f})",
+        )
+        table.add_row(
+            "triggered, failed",
+            f"{triggered_failed} ({triggered_failed / overall_count_triggered:2.5f})",
+        )
         print(table)
