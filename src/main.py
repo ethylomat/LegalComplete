09423dbd7210640ad8c83acd64cc85944e77f488
Otto Zastrow <otto.zastrow@hotmail.de>
2021-03-11 15:14:23 +0100

    adds terminal flag evaluate_trigger

--- src/main.py
+++ src/main.py
@@ -16,6 +16,11 @@ def parse_arguments():
         "--dont_eval", action="store_true", help="set to disable evaluation"
     )
     parser.add_argument(
+        "--evaluate_trigger",
+        action="store_true",
+        help="set to enable trigger evaluation after NGRAM training",
+    )
+    parser.add_argument(
         "--model_name", type=str, help="set model name eg. seq2seq or ngram"
     )
     parser.add_argument(
@@ -32,4 +37,9 @@ if __name__ == "__main__":
     if not args.dont_eval:
         metrics = c.evaluate_references(c.data_test)
         print_metrics(metrics)
+
+    if args.evaluate_trigger:
+        assert (
+            args.model_name == "NGRAM"
+        ), "trigger evaluation is only defined when using --model_name==NGRAM"
         c.evaluate_trigger(c.data_test)
